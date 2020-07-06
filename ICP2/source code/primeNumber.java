ad48a13d85a4cf3f13720424884bcae696dd4b43
Gabriella Willis <46979512+gabriellawillis@users.noreply.github.com>
2020-07-06 18:53:24 -0500

    Create primeNumber.java

---
+++ ICP2/source code/primeNumber.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class primeNumber {
+
+	public static class TokenizerMapper
+		extends Mapper<Object, Text, Text, IntWritable> {
+		
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(Object key, Text value, Context context
+				) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+	
+	public static class IntSumReducer
+	extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context
+				) throws IOException, InterruptedException {
+			int output = 0;
+			int num = Integer.parseInt(key.toString());
+		
+			for (int i = 2; i < num; i++) {
+				if(num % i == 0) {
+				output = 1;
+				}
+			}
+			result.set(output);
+			context.write(key, result);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(primeNumber.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+	
+
+}
